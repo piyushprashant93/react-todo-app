c21c2289da2beb9a2f322e333bd4f866c13ba66f
piyushprashant93 <piyushprashant93@gmail.com>
2021-12-04 13:09:53 +0530

    added no result state

---
+++ src/components/NoResults.js
@@ -0,0 +1,13 @@
+
+
+
+const NoResults = () => {
+    return (
+        <div className="no-results">
+            <h2>No Results</h2>
+        </div>
+    )
+
+}
+
+export default NoResults;
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -5,11 +5,11 @@ import AddNote from "./AddNote"
 import Searchbar from "./Searchbar"
 import Header from "./Header"
 const NoteList = () => {
-    const [searchText, setSearchText] = useState("")
+    const [noResult, setNoResults] = useState(false)
     const [note, setNote] = useState('');
     const [notes, setNotes] = useState([])
     const handleDelete = (id) => {
-        
+
         let newNotes = notes.filter(note => note.id !== id)
         setNotes(newNotes)
 
@@ -18,43 +18,55 @@ const NoteList = () => {
         if (note) {
             setNotes([...notes, { id: nanoid(), text: note, date: new Date().toISOString() }])
             setNote('')
-        }else{
+        } else {
             alert("Please add note")
         }
 
-     
+
     }
     const handleChanges = (e) => {
         setNote(e.target.value);
 
     }
     const handleSearch = (e) => {
-        setSearchText(e.target.value)
+        const search = e.target.value.toLowerCase();
+        const filteredNotes = notes.filter(note => note.text.toLowerCase().includes(search))
+        if (filteredNotes.length > 0) {
+            setNotes(filteredNotes)
+            setNoResults(false)
+        } else {
+            setNoResults(true)
+        }
+    }
+    const renderNotes = () => {
+        if (!noResult) {
+            return notes.map(note => <Notes key={note.id} note={note} handleDelete={handleDelete} />)
+        } 
     }
     return (
         <div className="bg-white dark:bg-black h-screen">
-             <div className="container mx-auto px-auto">
-           {/* Header */}
-             <Header/>
-            {/* Search bar */}
-            <Searchbar onChange={handleSearch}/>
+            <div className="container mx-auto px-auto">
+                {/* Header */}
+                <Header />
+                {/* Search bar */}
+                <Searchbar onChange={handleSearch} />
 
-            {/* Notes list  */}
-           < div className="grid grid-cols-auto-fit gap-4 my-12">
-            {
-                notes.filter(note=>note.text?.toLowerCase().includes(searchText)).map((note, index) => (
-                    <Notes key={index} note={note} onDelete={() => handleDelete(note.id)} />
-                ))
-            }
-            {/* Add new notes */}
-            <AddNote onAddNote={handleAddNotes} note={note} onChange={handleChanges} />
-          
-        </div>
-        </div>
+                {/* Notes list  */}
+                < div className="grid grid-cols-auto-fit gap-4 my-12">
+                    {
+
+                        renderNotes()
+                    }
+                    {/* Add new notes */}
+                    {!noResult && <AddNote onAddNote={handleAddNotes} note={note} onChange={handleChanges} />}
+
+                </div>
+                {noResult && <div className="text-center">No results found</div>}
+            </div>
 
         </div>
-       
-       
+
+
 
     )
 
